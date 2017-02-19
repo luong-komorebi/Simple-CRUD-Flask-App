9af9fa069acb56ae81f971eef42b35ca08845baf
LuongVo <vo.tran.thanh.luong@gmail.com>
2017-02-18 23:03:07 -0800

    Finish migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
+from flask_migrate import Migrate 
+
 
 # local import 
 from config import app_config
@@ -23,5 +25,9 @@ def create_app(config_name):
 	login_manager.init_app(app)
 	login_manager.login_message = "You must be logged in to access this page"
 	login_manager.login_view = "auth.login"
+
+	migrate = Migrate(app, db)
+	from app import models
+
 
 	return app
--- app/models.py
+++ app/models.py
@@ -63,17 +63,12 @@ class Department(db.Model):
 
 
 class Role(db.Model):
-   	"""
-    Create a Role table
-    """
 
-    __tablename__ = 'roles'
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(60), unique=True)
+	description = db.Column(db.String(200))
+	employees = db.relationship('Employee', backref='role', lazy='dynamic')
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True)
-    description = db.Column(db.String(200))
-    employees = db.relationship('Employee', backref='role',
-                                lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role: {}>'.format(self.name)
+	def __repr__(self):
+		return '<Role: {}>'.format(self.name)
