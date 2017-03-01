a2b98f17c0b19b00408972af442fac552294f44f
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-03-02 00:23:23 +0700

    Finished error handler pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # third party import
-from flask import Flask 
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 from flask_migrate import Migrate 
@@ -34,6 +34,24 @@ def create_app(config_name):
 
 	from .home import home as home_blueprint
 	app.register_blueprint(home_blueprint)
+
+	@app.errorhandler(403)
+	def forbidden(error):
+		return render_template('errors/403.html', title="Forbidden"), 403
+
+	@app.errorhandler(404)
+	def page_not_found(error):
+		return render_template('errors/404.html', title="Page Not Found"), 404
+
+	@app.errorhandler(500)
+	def internal_server_error(error):
+		 return render_template('errors/500.html', title='Server Error'), 500
+
+	@app.route('/500')
+	def error():
+		abort(500)
+
+
 
 
 
